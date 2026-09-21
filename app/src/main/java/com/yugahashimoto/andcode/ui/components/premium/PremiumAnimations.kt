b02126fb4164df41317105749612a1e6f0aae6f3
Chad <sahilsahilimtiyaz@gmail.com>
2026-09-21 20:06:30 +0530

    fix: compose drawBehind compilation error

--- app/src/main/java/com/yugahashimoto/andcode/ui/components/premium/PremiumAnimations.kt
+++ app/src/main/java/com/yugahashimoto/andcode/ui/components/premium/PremiumAnimations.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
